3ec01ff94846f49f7c5a720cfa66aae8e282f02f
Akrouch <guijakrouch@gmail.com>
2021-11-30 14:56:58 -0300

    added post, put, patch and delete methods to api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from profiles_api import serializers
+
+
+# vagrant up
+# vagrant ssh
+# cd ./vagrant
+# source ~/env/bin/activate
+# python manage.py runserver 0.0.0.0:8000
+
+class HelloApiView(APIView):
+    """Test API View"""
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+
+        an_apiview = [
+            'baga',
+            'pijas',
+            'fei'
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request, format=None):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        #pk is primary key to find the object to update
+        """Handle updating an object"""
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'Method': 'DELETE'})
